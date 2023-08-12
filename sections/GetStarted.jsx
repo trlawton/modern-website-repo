35aac31acfad37f82924fff4cc97b28edd259c27
trlawton <rlawton.photo@gmail.com>
2023-08-12 01:44:54 -0700

    6th commit, completed Whats New and World components

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -12,30 +12,36 @@ const GetStarted = () => (
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
-      viewport={{ once: "false", amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto flex leg:flex-row flex-col gap-8`}
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
     >
-      <motion.dv
-        variants={planetVariants("left")}
+      <motion.div
+        variants={planetVariants('left')}
         className={`flex-1 ${styles.flexCenter}`}
       >
-        <img src="/get-started.png" alt="get-started" className="w-[90%] h-[90%]
-          object-contain" />
-      </motion.dv>
+        <img
+          src="/get-started.png"
+          alt="get-started"
+          className="w-[90%] h-[90%] object-contain"
+        />
+      </motion.div>
       <motion.div
-        variants={fadeIn("left", "tween", 0.2, 1)}
+        variants={fadeIn('left', 'tween', 0.2, 1)}
         className="flex-[0.75] flex justify-center flex-col"
       >
         <TypingText title="| How Metaversus Works" />
         <TitleText title={<>Get started with just a few clicks</>} />
         <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]">
           {startingFeatures.map((feature, index) => (
-            <StartSteps key={feature} number={index + 1} text={feature} />
+            <StartSteps
+              key={feature}
+              number={`${index < 10 ? '0' : ''} ${index + 1}`}
+              text={feature}
+            />
           ))}
         </div>
       </motion.div>
     </motion.div>
-
   </section>
 );
 
--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,10 +1,10 @@
 'use client';
 
 import { motion } from "framer-motion";
-import styles from "..styles";
+import styles from "../styles";
 import { staggerContainer, fadeIn, planetVariants } from "../utils/motion";
 import { NewFeatures, TitleText, TypingText } from "../components";
-import { newFeatures } from "../contstants";
+import { newFeatures } from "../constants";
 
 const WhatsNew = () => (
   <section className={`${styles.paddings} relative z-10`}>
@@ -12,12 +12,12 @@ const WhatsNew = () => (
       variants={staggerContainer}
       initial="hidden"
       whileInView="show"
-      viewport={{ once: "false", amount: 0.25 }}
-      className={`${styles.innerWidth} mx-auto flex leg:flex-row flex-col gap-8`}
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
     >
       <motion.div
         variants={fadeIn("right", "tween", 0.2, 1)}
-        className="flex-[0.75] flex justify-center flex-col"
+        className="flex-[0.95] flex justify-center flex-col"
       >
         <TypingText title="| What's New?" />
         <TitleText title={<>What's new about Metaversus?</>} />
